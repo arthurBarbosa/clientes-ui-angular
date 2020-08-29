a3c96e6220e89d688fd2a2f75599ba6889d2a417
Arthur Barbosa <arthurbruno03@gmail.com>
2020-08-29 15:43:13 -0400

    criacao do cadastro de cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from  '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClientesService } from './clientes.service';
 import { ClientesModule } from './clientes/clientes.module';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
@@ -16,9 +17,12 @@ import { HomeComponent } from './home/home.component'
     BrowserModule,
     AppRoutingModule,
     ClientesModule,
-    TemplateModule
+    TemplateModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cliente } from '../cliente';
+import { ClientesService } from 'src/app/clientes.service';
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente;
+  success: boolean = false;
+  errors: string[];
+
+  constructor(private service: ClientesService) {
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.service
+      .salvar(this.cliente)
+      .subscribe(response => {
+        this.success = true;
+        this.errors = null;
+        this.cliente = response;
+      },
+        errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        }
+      )
+  }
 }
