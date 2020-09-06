d80622baf6f3824ab1e70a15c31d210784d1af58
Arthur Barbosa <arthurbruno03@gmail.com>
2020-09-06 19:59:11 -0400

    cadastro de usuario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from '../login/usuario';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +12,45 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean = false;
+  mensagemSucesso: string = '';
+  errors: string[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
+  onSubmit() {
     this.router.navigate(['home']);
   }
 
-  preparaCadastro(event){
+  preparaCadastro(event) {
     event.preventDefault();
     this.cadastrando = true;
-  }  
+  }
 
-  cancelaCadastro(){
+  cancelaCadastro() {
     this.cadastrando = false;
+  }
+
+  cadastrar() {
+
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+
+    this.authService.salvar(usuario)
+      .subscribe(response => {
+        this.mensagemSucesso = "Cadastro realizado com sucesso!"
+
+      }, errorResponse => {
+        this.mensagemSucesso = null;
+        this.errors = errorResponse.error.errors
+      })
   }
 
 }
